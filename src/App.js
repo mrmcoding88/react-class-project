98387bd422def7c90a531d24d4b6328d2d6e892a
Reza <mohammad.r.moradi@gmail.com>
2022-10-28 20:22:04 +0200

    chore: initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+
+const Logo = () =>{
+  return(
+    <header>
+    <h1>Welcome to the Pokedex</h1>
+    <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png'></img>
+    </header>)
 }
+ const BestPokemone = ()=>(
+ <p>My favorite Pokémon is Squirtle</p>
+ )
+
+
+function App(){
+  return (<div>
+    <Logo />
+  <BestPokemone />
+  </div>)
+}
+
 
 export default App;
